8d6a4a3d8d289c7cea795e267a5019ddf10531d3
Raphaël Assénat <raph@raphnet.net>
2015-07-24 01:42:55 +0000

    Copyright update

--- twelve.c
+++ twelve.c
@@ -1,5 +1,5 @@
-/* Nes/Snes/Genesis/SMS/Atari to USB
- * Copyright (C) 2006-2007 Raphaël Assénat
+/* Twelve input to USB (4 directions + 8 buttons) to USB
+ * Copyright (C) 2008-2015 Raphaël Assénat
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
